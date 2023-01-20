4525caa47bc8f38cb294cd2cb43614f6977c4d1b
pauldubois98 <pauldubois98@gmail.com>
2023-01-20 11:36:44 +0100

    evolution of loss across iterations

--- scripts/learning_rates_plots.py
+++ scripts/learning_rates_plots.py
@@ -53,3 +53,41 @@ plot_lr(0.5, 'Learning rate too low')
 plot_lr(0.8, 'Good learning rate')
 plot_lr(3, 'Learning rate too large')
 plot_lr(4, 'Learning rate much too large')
+
+
+
+####################### Plotting the loss function #######################
+
+def iters_lr(alpha=1, max_it=16):
+    # Initialize starting point and step size
+    x = torch.tensor([-3.], requires_grad=True)
+
+    # Create an empty list to store the values of x at each step
+    x_list = []
+
+    # Perform gradient descent
+    for i in range(max_it):
+        x_list.append(x.item())
+        y = loss(x)
+        y.backward()
+        x.data = x.data - alpha*x.grad.data
+        x.grad.data.zero_()
+    y_list = loss(torch.tensor(x_list)).detach().numpy()
+    return y_list
+
+
+from matplotlib.pyplot import figure
+figure(figsize=(8, 6), dpi=80)
+
+MAX_IT = 25
+plt.plot(np.arange(MAX_IT), iters_lr(alpha=0.2, max_it=MAX_IT), label='too low')
+plt.plot(np.arange(MAX_IT), iters_lr(alpha=0.7, max_it=MAX_IT), label='good')
+plt.plot(np.arange(MAX_IT), iters_lr(alpha=2, max_it=MAX_IT), label='too large')
+plt.plot(np.arange(MAX_IT), iters_lr(alpha=10, max_it=MAX_IT), label='way too large')
+plt.legend(prop={'size': 16})
+plt.xticks(np.arange(MAX_IT))
+plt.xlabel(r"Iteration", fontsize=16)
+plt.ylim(0,9)
+plt.ylabel(r"$\mathcal{L}(\omega$)", fontsize=16)
+plt.savefig('loss_per_iter.svg', format="svg")
+plt.show()
