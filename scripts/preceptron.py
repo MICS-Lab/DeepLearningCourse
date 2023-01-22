f792622affa344a54a0b43238f4796677d7429c4
pauldubois98 <pauldubois98@gmail.com>
2023-01-22 22:43:00 +0100

    preceptron diagram script

---
+++ scripts/preceptron.py
@@ -0,0 +1,29 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+import os
+from ann_visualizer.visualize import ann_viz
+
+file_path = os.path.realpath(__file__)
+os.chdir(file_path.replace("preceptron.py", ''))
+
+rectangle_network = Sequential()
+rectangle_network.add(Dense(units=6,
+                  activation='relu',
+                  kernel_initializer='uniform',
+                  input_dim=3))
+rectangle_network.add(Dense(units=8,
+                  activation='relu',
+                  kernel_initializer='uniform'))
+rectangle_network.add(Dense(units=7,
+                  activation='relu',
+                  kernel_initializer='uniform'))
+rectangle_network.add(Dense(units=5,
+                  activation='relu',
+                  kernel_initializer='uniform'))
+rectangle_network.add(Dense(units=2,
+                  activation='sigmoid',
+                  kernel_initializer='uniform'))
+rectangle_network.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+ann_viz(rectangle_network, title="wide", filename="preceptron.gv")
