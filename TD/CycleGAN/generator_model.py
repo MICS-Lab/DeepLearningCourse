b49cd5615dd088c1b3cfa378e84b3d6c199831cf
JasonMendoza2008 <lhotteromain@gmail.com>
2023-03-28 15:27:24 +0200

    Generator coded

--- TD/CycleGAN/generator_model.py
+++ TD/CycleGAN/generator_model.py
@@ -18,8 +18,20 @@ class Block(nn.Module):
         return self.conv(x)
 
 
+class ResBlock(nn.Module):
+    def __init__(self, num_channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            Block(in_channels = num_channels, out_channels = num_channels, down=True, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
+
 class Generator(nn.Module):
-    def __init__(self, img_channels: int = 3, num_features: int = 64, num_residuals: int = 6):
+    def __init__(self, img_channels: int = 3, num_features: int = 64):
         super().__init__()
         self.initial = nn.Sequential(
             nn.Conv2d(
@@ -33,7 +45,23 @@ class Generator(nn.Module):
             nn.InstanceNorm2d(num_features),
             nn.ReLU(inplace=True)
         )
-        self.last(nn.Sequential(
+        self.down_blocks = nn.ModuleList([
+            Block(num_features, num_features*2, down=True, kernel_size=3, stride=2, padding=1),
+            Block(num_features*2, num_features*4, down=True, kernel_size=3, stride=2, padding=1)
+        ])
+        self.res_blocks = nn.ModuleList([
+            ResBlock(num_features*4),
+            ResBlock(num_features*4),
+            ResBlock(num_features*4),
+            ResBlock(num_features*4),
+            ResBlock(num_features*4),
+            ResBlock(num_features*4),
+        ])
+        self.up_blocks = nn.ModuleList([
+            Block(num_features*4, num_features*2, down=False, kernel_size=3, stride=2, padding=1, output_padding=1),
+            Block(num_features*2, num_features, down=False, kernel_size=3, stride=2, padding=1, output_padding=1),
+        ])
+        self.last = nn.Sequential(
             nn.Conv2d(
                 num_features,
                 img_channels,
@@ -42,7 +70,32 @@ class Generator(nn.Module):
                 padding=3,
                 padding_mode="reflect",
             )
-        ))
+        )
 
     def forward(self, x):
-        pass
+        x = self.initial(x)
+        for layer in self.down_blocks:
+            x = layer(x)
+        for layer in self.res_blocks:
+            x = layer(x)
+        for layer in self.up_blocks:
+            x = layer(x)
+        x = self.last(x)
+        return torch.tanh(x)
+
+
+def test():
+    img_channels = 3
+    img_size = 256
+    x = torch.randn((2, img_channels, img_size, img_size))
+    print(x.shape)
+
+    generator = Generator()
+    print(generator(x).shape)
+
+
+if __name__ == "__main__":
+    test()
+
+
+
