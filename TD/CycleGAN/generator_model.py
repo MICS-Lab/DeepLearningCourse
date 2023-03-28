43a64110a11c048318edec2b2dda1fbd194f3c41
JasonMendoza2008 <lhotteromain@gmail.com>
2023-03-28 15:08:36 +0200

    Class Blcok

--- TD/CycleGAN/generator_model.py
+++ TD/CycleGAN/generator_model.py
@@ -5,7 +5,17 @@ import torch.nn as nn
 
 
 class Block(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, down: bool, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs) if down
+            else nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace = True)
+        )
 
+    def forward(self, x):
+        return self.conv(x)
 
 
 class Generator(nn.Module):
@@ -34,3 +44,5 @@ class Generator(nn.Module):
             )
         ))
 
+    def forward(self, x):
+        pass
